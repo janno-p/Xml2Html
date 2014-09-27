07f38629f56b5b5a3521d251e626a5c601ae085d
Janno Põldma <janno.poldma@gmail.com>
2014-09-27 10:09:29 +0300

    Suave hello world application.

--- Xml2Html/Program.fs
+++ Xml2Html/Program.fs
@@ -1,7 +1,20 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿module Xml2Html.Program
 
+open Suave.Log
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Successful
+open Suave.Web
+
+let logger = Loggers.sane_defaults_for Debug
+
+let application =
+    choose [
+        log logger log_format >>= never
+        url "/hello" >>= never >>= OK "Never executes"
+        url "/hello" >>= OK "Hello, World!"
+        url "/quit" >>= warbler (fun _ -> System.Environment.Exit(0); never)
+        RequestErrors.NOT_FOUND "Found no handlers"
+    ]
+
+web_server default_config application
