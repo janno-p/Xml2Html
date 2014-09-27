e5ffca3d0aeb45552f478916d9f785536dfdf1ad
Janno Põldma <janno.poldma@gmail.com>
2014-09-27 09:25:33 +0300

    Initial F# console application.

---
+++ Xml2Html/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module Xml2Html.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("Xml2Html")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("janno")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
